860df995418291fbb530735a47d1baea7d828c26
JD <jaysondorilag1@gmail.com>
2025-03-17 23:33:01 +0800

    Refactor mailtrap configuration; enable Gmail transporter and comment out Mailtrap setup

--- config/mailtrapConfig.js
+++ config/mailtrapConfig.js
@@ -3,15 +3,15 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-// const transporter = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: process.env.GMAIL_USER,
-//     pass: process.env.GMAIL_APP_PASSWORD 
-//   }
-// });
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_APP_PASSWORD 
+  }
+});
 
-// module.exports = transporter;
+module.exports = transporter;
 
 //use this for mail trap
 // const nodemailer = require('nodemailer');
@@ -19,13 +19,13 @@ dotenv.config();
 
 // dotenv.config();
 
-const transporter = nodemailer.createTransport({
-  host: process.env.MAILTRAP_HOST,
-  port: process.env.MAILTRAP_PORT,
-  auth: {
-    user: process.env.MAILTRAP_USER,
-    pass: process.env.MAILTRAP_PASS
-  }
-});
+// const transporter = nodemailer.createTransport({
+//   host: process.env.MAILTRAP_HOST,
+//   port: process.env.MAILTRAP_PORT,
+//   auth: {
+//     user: process.env.MAILTRAP_USER,
+//     pass: process.env.MAILTRAP_PASS
+//   }
+// });
 
-module.exports = transporter;
+// module.exports = transporter;
